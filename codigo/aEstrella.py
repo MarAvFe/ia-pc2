83d4a9b91c73d300948c9622657d1b626a536058
sfalconc <37492086+sfalconc@users.noreply.github.com>
2018-05-20 20:08:33 +0200

    Add files via upload

--- codigo/aEstrella.py
+++ codigo/aEstrella.py
@@ -2,19 +2,6 @@ import random
 import shutil
 import os
 
-class Estado(object):
-	g = 0
-	h = 0
-	f = 0
-
-	def __init__(self, g, h, f):
-		self.g = g
-		self.h = h
-		self.f = f
-
-def getEstado(g, h, f):
-	estado = Estado(g, h, f)
-	return estado
 
 # Busca y encuentra las coordenadas de todas las zanahorias en el tablero visible
 def encontrarZanahorias(tablero):
@@ -22,7 +9,7 @@ def encontrarZanahorias(tablero):
 	for y in range(len(tablero)):
 		for x in range(len(tablero[y])):
 			if (tablero[y][x] == "Z"):
-				posicionZanahoriasappend([x, y])	
+				posicionZanahorias.append([y, x])	
 	return posicionZanahorias
 
 # Encuentra las coordenadas del conejo
@@ -30,7 +17,7 @@ def encontrarConejo(tablero):
 	for y in range(len(tablero)):
 		for x in range(len(tablero[y])):
 			if tablero[y][x] == "C":
-				return (x,y)
+				return (y,x)
 	return (-1,-1)
 
 # Leer tablero inicial
@@ -82,9 +69,10 @@ def guardarArchivo(tablero, nombre):
 			archivo.write(lTmp)
 
 # Mueve al conejo al conejo en el tablero según el resultado ganador
-def movimiento(posicionConejo, movimientoGanador, tablero):
+def moverConejoTablero(posicionConejo, movimientoGanador, tablero):
 	f, c = posicionConejo
-	tablero[f][c] = "" # Elimina al conejo de la posición en la que se encontraba
+	print("posicion del conejo: "+","+ str(f)+ ","+ str(c))
+	tablero[f][c] = " " # Elimina al conejo de la posición en la que se encontraba
 	comioZanahoria = False
 	if movimientoGanador == "IZQUIERDA":
 		c-=1
@@ -100,7 +88,7 @@ def movimiento(posicionConejo, movimientoGanador, tablero):
 	# Coloca al conejo en la posición nueva.
 	tablero[f][c] = "C"
 	# Devuelve: nueva coordenada del conejo, si comió una zanahoria, 
-	return (f,c), comioZanahoria, posicionConejo
+	return comioZanahoria, tablero
 
 #def moverConejo(tablero, posicionConejo, nuevaPosicion):
 #	f_c, c_c = posicionConejo
@@ -138,11 +126,11 @@ def getMejorMovimiento(listaF):
 	return movimientos[listaF.index(min(listaF))]
 
 def imprimirCostosDeCadaMovimiento(paso, costoPorMovimiento, movimientoGanador):
-	print("PASO: "+ str(paso).zfill(5) + " IZQUIERDA: "+ costoPorMovimiento[0] +
-										" DERECHA: " + costoPorMovimiento[1] +
-										" ARRIBA: " + costoPorMovimiento[2] +
-										" ABAJO: "+ costoPorMovimiento[3]  + 
-										" MOVIMIENTO: "+ movimientoGanador)
+	print("PASO: "+ str(paso).zfill(5) + " IZQUIERDA: "+ str(costoPorMovimiento[0]) +
+										" DERECHA: " + str(costoPorMovimiento[1]) +
+										" ARRIBA: " + str(costoPorMovimiento[2]) +
+										" ABAJO: "+ str(costoPorMovimiento[3])  + 
+										" MOVIMIENTO: "+ str( movimientoGanador))
 	#PASO: 00001 IZQUIERDA: 5 DERECHA: 3 ARRIBA: 8 ABAJO: 9 MOVIMIENTO: DERECHA
 
 
@@ -173,21 +161,15 @@ def calcularCostoTotal(posicionConejo, tableroVisible):
 
 
 
-def calcularHeuristico():
-	pass
-
-def encontrarZanahoria():
-	pass
-
-def calcularCosto():
-	# Espacio vacío = 0
-	# Zanahoria = 1
-	pass
-
 
 def getTableroVisible(tablero, rangoVision, posicionConejo):
 	tableroVisible = []
-	izquierda, derecha, arriba, abajo = False
+	izquierda = False
+	derecha = False
+	arriba = False
+	abajo = False
+	x_ConejoVisible=-1
+	y_ConejoVisible=-1
 	f_c, c_c = posicionConejo
 	c_min = c_c - rangoVision
 	f_min = f_c - rangoVision
@@ -212,7 +194,19 @@ def getTableroVisible(tablero, rangoVision, posicionConejo):
 		for c in range(c_min,c_max+1):
 			lTemp.append (tablero [f][c])
 		tableroVisible.append(lTemp)
-	return tableroVisible,  izquierda, derecha, arriba, abajo
+	nFilas = 0
+	nColumnas = 0
+	for i in tableroVisible:
+		for j in i:
+			if j=="C":
+				x_ConejoVisible=nFilas
+				y_ConejoVisible=nColumnas
+			nColumnas+=1
+		nColumnas=0
+		nFilas+=1
+
+	print("posicionConejoVisible_x: "+ str(x_ConejoVisible)+", posicionConejoVisible_y: "+ str(y_ConejoVisible))
+	return (x_ConejoVisible,y_ConejoVisible), tableroVisible,  izquierda, derecha, arriba, abajo
 
 def imprimirTableroOriginal(tablero):
 	for f in range(len(tablero)):
@@ -259,6 +253,7 @@ def cantidadZanahoriasTablero(tablero):
 	return totalZanahorias
 
 def getVecinos(posicionConejo, tablero):
+	print("Posición del conejo: "+ str( posicionConejo[0])+", "+str(posicionConejo[1]))
 	listaVecinos = []
 	costoMovimiento=[]
 	coordenadas =[(0,-1),(0,1),(-1,0),(1,0)]
@@ -269,7 +264,7 @@ def getVecinos(posicionConejo, tablero):
 		_x = x + i[0]
 		_y = y + i[1]
 		if (_x>=0 and _y >= 0 and _x <= numeroFilas and _y <= numeroColumnas):
-			listaVecinos.append[_x, _y]
+			listaVecinos.append([_x, _y])
 	return listaVecinos
 
 def penalizarAnterior(eAnterior, eActual):
@@ -288,18 +283,28 @@ def movimiento(x,y):
 	elif(x == 1 and y == 0): # Abajo
 		return "ABAJO", 1		
 
-def grupoZanahorias(tablero, vecinos):
+def grupoZanahorias(tablero, v, zanahorias):
 	totalZanahorias = len(zanahorias)
 	conta = 0
 	pesos = []
+	tAncho = len(tablero[0])
+	tAlto = len(tablero)
+	print(str(tAlto)+","+str(tAncho))
 	tmpZanahorias = 0
 	x_v, y_v = v[0]
+	print("x_v: "+ str(x_v))
+	print("y_v: "+ str(y_v))
 	for f in tablero: # Izquierda
 		for c in f:
+			
 			if (conta <= y_v ):
+				print ("C:"+str(c))
 				if (c=="Z"):
 					tmpZanahorias+=1
 			conta+=1
+		print("\n")
+		conta=0
+	print("total zanahorias: "+ str(totalZanahorias)+"tmpzanahorias: "+str(tmpZanahorias))
 	pesos.append(totalZanahorias-tmpZanahorias)
 	conta = 0
 	tmpZanahorias=0
@@ -310,29 +315,51 @@ def grupoZanahorias(tablero, vecinos):
 				if (c=="Z"):
 					tmpZanahorias+=1
 			conta+=1
+		conta=0
 	pesos.append(totalZanahorias-tmpZanahorias)
-	conta = 0
+	#conta = 0
+
 	tmpZanahorias=0
+	print("arriba 2: ")
 	x_v, y_v = v[2] # Arriba
+	print("x_v: "+ str(x_v))
+	print("y_v: "+ str(y_v))
+	conta = x_v
+	print("conta: "+str(conta))
 	for f in tablero: 
+		print("iteración")
 		for c in f:
-			if (conta <= x_v ):
+			if (conta <= x_v and conta >=0):
+				print ("C:"+str(c)+", contador: "+str(conta))
 				if (c=="Z"):
 					tmpZanahorias+=1
-			conta+=1
+			else:
+				break
+		conta-=1
 	pesos.append(totalZanahorias-tmpZanahorias)
-	conta = 0
+
 	tmpZanahorias=0
+	print("abajo: ")
 	x_v, y_v = v[3] # Abajo
+	print("x_v: "+ str(x_v))
+	print("y_v: "+ str(y_v))
+	conta = 0
+	print("conta: "+str(conta))
 	for f in tablero: 
+		print("iteración")
 		for c in f:
-			if (conta >= x_v ):
+			if (conta >= x_v):
+				print ("C:"+str(c)+", contador: "+str(conta))
 				if (c=="Z"):
 					tmpZanahorias+=1
-			conta+=1
+			else:
+				break
+		conta+=1
 	pesos.append(totalZanahorias-tmpZanahorias)
 	return pesos
+
 def penalidadPared(paredIzq,paredDer, paredArr, paredAba):
+	print(">>>>>>>>>>>>>>>>>> penalidad por chocar con pared <<<<<<<<<<<<<<<<< ")
 	penalidades = []
 	if (paredIzq==True):
 		penalidades.append(15)
@@ -350,7 +377,20 @@ def penalidadPared(paredIzq,paredDer, paredArr, paredAba):
 		penalidades.append(15)
 	else:
 		penalidades.append(0)	
+	return penalidades
 
+def penalizarEstadoAnterior(ultimoMovimientoGanador, vecinos):
+	peso=15
+	penalizacion = [0,0,0,0]
+	if (ultimoMovimientoGanador=="IZQUIERDA"):
+		return [0,peso,0,0]#Penalizar DERECHA
+	elif (ultimoMovimientoGanador=="DERECHA"):
+		return [peso,0,0,0]#Penalizar IZQUIERDA	
+	elif (ultimoMovimientoGanador=="ARRIBA"):
+		return [0,0,0, peso]#Penalizar ABAJO	
+	elif (ultimoMovimientoGanador=="ABAJO"):
+		return [0,0,peso,0]#Penalizar ARRIBA	
+	return penalizacion
 
 
 
@@ -358,8 +398,11 @@ def penalidadPared(paredIzq,paredDer, paredArr, paredAba):
 def main(rangoVision = 1, zanahoriasPorComer = 3):
 	abierta = []
 	cerrada = []
-	f, lG = [0,0,0,0]
-	g, h = 0
+	f = [0,0,0,0]
+	lG = [0,0,0,0]
+	g = 0
+	h = 0
+	ultimoMovimientoGanador=""
 	zanahoriasComidas = 0
 	costoAcumulado = 0
 	estadoAnterior=[-1, -1]
@@ -369,61 +412,101 @@ def main(rangoVision = 1, zanahoriasPorComer = 3):
 	print("********** Tablero inicial **********")
 	tablero = leerTableroInicial("tableroInicial.txt")	
 	imprimirTablero(tablero)
-	cerrada.append(posicionConejo = encontrarConejo(tablero))
+	posicionConejo = encontrarConejo(tablero)
 	cantidadInicialZanahorias = cantidadZanahoriasTablero (tablero)
-	posicionAnteriorConejo = posicionConejo # Inicial, en este caso
+	posicionAnterior = posicionConejo # Inicial, en este caso
 	zanahoraisComidas = 0
 	# Revisa si es que las condiciones son válidas, si se puede iniciar.
 	if ((cantidadInicialZanahorias < zanahoriasPorComer) or (posicionConejo[0]==-1 and posicionConejo[1]==-1)):
 		print ("Error al ingresar los parámetros")
 	else:
 		#guardarArchivo(tablero, str(paso).zfill(5))
-		abierta = getVecinos(posicionConejo, tableroVisible)
-		while(zanahoriasPorComer != zanahoraisComidas):
+		while(zanahoriasPorComer != zanahoriasComidas):
 			lH = [0,0,0,0]
 			paso+=1
+			posicionConejo = encontrarConejo(tablero)
 			# Busca los espacios visibles
-			tableroVisible, paredIzq, paredDer, paredArr, paredAba = getTableroVisible(tablero, rangoVision, posicionConejo)
+			posicionConejoVisible, tableroVisible, paredIzq, paredDer, paredArr, paredAba = getTableroVisible(tablero, rangoVision, posicionConejo)
+			vecinos = getVecinos(posicionConejoVisible, tableroVisible)
+
 			print("-------------------- Tablero visible --------------------")
 			imprimirTablero(tableroVisible)
 			lG = [x+g for x in lG] # Suma g a cada uno de los futuros estados
 			coordenadasZanahorias = encontrarZanahorias(tableroVisible) # Obtiene las coordenadas de las zanahorias
-			if (estadoAnterior[0]!= -1 and estadoAnterior[1] != -1):
-				lH[penalizarEstadoAnterior(estadoAnterior, posicionConejo)] = 15 #penaliza el estado anterior
-			print("PENALIDAD ESTADO ANTERIOR "+"IZQUIERDA: " + lH[0]+ "DERECHA: " + lH[1] +"ARRIBA: " + lH[2]+"ABAJO: " + lH[3])
+
+			# Penalizar para que no regrese a posición a la posición de donde viene
+			if (ultimoMovimientoGanador!=""):
+				p0 = penalizarEstadoAnterior(ultimoMovimientoGanador, vecinos)
+				print("PENALIDAD ESTADO ANTERIOR "+"IZQUIERDA: " + str(p0[0])+ "DERECHA: " + 
+				str(p0[1]) +"ARRIBA: " + str(p0[2])+"ABAJO: " + str(p0[3]))
+				lH = [x + y for x, y in zip(lH, p0)]
+
+			# Penalizar para que no choque con la pared
+			p3 = penalidadPared(paredIzq,paredDer, paredArr, paredAba) #falta revisar
+			print("PENALIDAD PARED:  "+"IZQUIERDA: " + str(p3[0])+ ", DERECHA: " + 
+			str(p3[1]) +", ARRIBA: " + str(p3[2])+", ABAJO: " + str(p3[3]))				
+			lH = [x + y for x, y in zip(lH, p3)]
+
 			# Revisa si hay zanahorias en el tablero visible
 			if (len(coordenadasZanahorias)!= 0):
 				# Hay zanahorias a la vista
 				# Sacar costo de la distancia del conejo a la zanahoria más cercana
-				p1 = zanahoriaMasCercana(zanahorias, vecinos)
-				lH = [x + y for x, y in zip(lH, p1)]
-				print("PENALIDAD ZANAHORIA CERDA: "+"IZQUIERDA: " + lH[0]+ "DERECHA: " + lH[1] +"ARRIBA: " + lH[2]+"ABAJO: " + lH[3])
-				p2 = grupoZanahorias(tableroVisible, vecinos)
+				p1 = zanahoriaMasCercana(coordenadasZanahorias, vecinos)
+				lH = [x + y for x, y in zip(lH, p1)] #suma la penalidad anterior al heurístico
+				print("* COORDENADAS VECINOS: "+ str(vecinos[0][0])+","+str(vecinos[0][1])+", "
+					+str(vecinos[1][0])+","+str(vecinos[1][1])+
+					","+str(vecinos[2][0])+ ","+str(vecinos[2][1])+
+					","+str(vecinos[3][0])+","+str(vecinos[3][1]))
+				print("PENALIDAD zanahoria cercana: "+"IZQUIERDA: " + str(p1[0])+ ", DERECHA: " + 
+				str(p1[1]) +", ARRIBA: " + str(p1[2])+", ABAJO: " + str(p1[3]))		
+				# Calcula la penalidad según grupo de zanahorias que tenga cerca		
+				p2 = grupoZanahorias(tableroVisible, vecinos, coordenadasZanahorias)
 				lH = [x + y for x, y in zip(lH, p2)]
-				print("PENALIDAD GRUPO ZANAHORIAS"+"IZQUIERDA: " + lH[0]+ "DERECHA: " + lH[1] +"ARRIBA: " + lH[2]+"ABAJO: " + lH[3])
-				p3 = penalidadPared(paredIzq,paredDer, paredArr, paredAba)
-				lH = [x + y for x, y in zip(lH, p2)]
-				print("PENALIDAD PARED: "+"IZQUIERDA: " + lH[0]+ "DERECHA: " + lH[1] +"ARRIBA: " + lH[2]+"ABAJO: " + lH[3])
+				print("PENALIDAD Grupo zanahorias:  "+"IZQUIERDA: " + str(p2[0])+ ", DERECHA: " + 
+				str(p2[1]) +", ARRIBA: " + str(p2[2])+", ABAJO: " + str(p2[3]))
 
 				f = [x + y for x, y in zip(lH, lG)]
+				print("F:  "+"IZQUIERDA: " + str(f[0])+ ", DERECHA: " + 
+				str(f[1]) +", ARRIBA: " + str(f[2])+", ABAJO: " + str(f[3]))		
+				
+
 				mejorMovimiento = getMejorMovimiento(f)
+				ultimoMovimientoGanador=mejorMovimiento
 				# Imprimir todos los costos y mejro movimiento
 				imprimirCostosDeCadaMovimiento(paso, f, mejorMovimiento)
-				# Guardar posición actual como posición anterior
-				# Hacer el movimiento
+#				posicionAnterior= encontrarConejo(tablero)
+				print("********** tablero antes de movimiento")
+				imprimirTablero(tablero)
+				dd,gg = encontrarConejo(tablero)
+				print("posicion conejo tablero original antes de moverlo: "+"-->" +str(dd)+","+str(gg))
+				comioZanahoria, tablero = moverConejoTablero(encontrarConejo(tablero), mejorMovimiento, tablero)
+				# Revisa si se comió una zanahoria en el movimiento
+				if (comioZanahoria== True):
+					zanahoriasComidas+=1
 
+
+			# No hay zanahorias en el tablero visible	
 			else:
+				print("**************** NO HAY ZANAHORIAS VISIBLES, SE HACE UN MOVIMIENTO ALEATORIO ***************************")
+				#ultimoMovimientoGanador=
+				f = [x + y for x, y in zip(lH, lG)]
+				print("F:  "+"IZQUIERDA: " + str(f[0])+ ", DERECHA: " + 
+				str(f[1]) +", ARRIBA: " + str(f[2])+", ABAJO: " + str(f[3]))	
+				mejorMovimiento = getMejorMovimiento(f)
+				ultimoMovimientoGanador=mejorMovimiento				
+				comioZanahoria, tablero = moverConejoTablero(encontrarConejo(tablero), mejorMovimiento, tablero)
 
-				# No se ven zanahorias en el tablero.
+				# No se ven zanahorias en el tablero. Movimiento aleatorio, pero no regresar a la posición anterior
 
-
-
-			# Calcular el costo de cada movimiento
-			calcularMejorMovimiento(posicionConejo, tableroVisible, posicionAnterior, tablero)
-			# Imprime resultado
-			imprimirCostosDeCadaMovimiento(paso , "FALTA", "FALTA")
+			g+=1
+			print("*********** TABLERO VISIBLE")
+			imprimirTablero(tableroVisible)
+			print("-> CANTIDAD DE ZANAHORIAS COMIDAS: " + str(zanahoriasComidas))
+			print("-> CANTIDAD DE ZANAHORIAS POR COMER: " + str(zanahoriasPorComer))	
+			input("PRESIONAR UNA TECLA PARA EL SIGUIENTE MOVIMIENTO") 
+			guardarArchivo(tablero, str(paso).zfill(5))
 			
-		print("PASO: "+ getNumeroPaso+ " FINAL")
+	print("PASO: "+ str(paso).zfill(5) + " FINAL")
 
 
 main(2, 2)
